e59684077d87fe05f12b67f9fb8aa13f49d8b578
alfarishi-git <ralfarishi@gmail.com>
2023-09-06 16:42:59 +0700

    update: url link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://blog-alfarishi.vercel.app",
+	url: "https://blog.alfarishi.my.id",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/",
